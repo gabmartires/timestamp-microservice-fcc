1a12d861c3d681f56f2feb491360a9609734e612
Gabriel Martires <gmartires23@gmail.com>
2023-12-12 11:41:36 -0600

    Corrected Code Review

--- index.js
+++ index.js
@@ -19,37 +19,34 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+//Validating Date Function
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
+
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+ let date = new Date(req.params.date)
+ 
+ if (isInvalidDate(date)) {
+   date = new Date(+req.params.date)
+ }
+
+ if (isInvalidDate(date)) {
+  res.json({error: "Invalid Date"})
+  return;
+ }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 app.get("/api", (req, res) => {
-  res.json({
-    unix: new Date().getTime(),
-    utc: new Date().toUTCString(),
-  });
-});
-
-app.get("/api/:timestamp", (req, res) => {
-  const timestamp = req.params.timestamp;
-
- if(!isNaN(Number(timestamp)) && timestamp.length === 13) {
-  return res.json({
-    unix: timestamp,
-    utc: new Date(Number(timestamp)).toUTCString(),
-  });
- }
-
- if(new Date(timestamp).toUTCString() != "invalid Date") {
-  return res.json({
-    unix: new Date(timestamp).getTime(),
-    utc: new Date(timestamp).toUTCString(),
-  });
- }
-
-  res.json({ error: 'Invalid Date' });
-});
+ res.json({
+  unix: new Date().getTime(),
+  utc: new Date().toUTCString()
+ })
+})
 
 // listen for requests :)
 //var listener = app.listen(process.env.PORT, function () {
